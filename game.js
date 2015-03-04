7ba6732e47fe098e08eb90e2f4589ea686365aaa
Eric Hambro <eric.hambro@gmail.com>
2015-03-04 14:38:16 -0500

    whitespace & semicolons

--- game.js
+++ game.js
@@ -17,13 +17,12 @@
         this.fruit = [];
 
         this.isOver = false;
-        this.endTicker =0
-
+        this.endTicker =0;
+        
         var self = this;
 
         var tick = function(){  
             
-
             if (self.isOver === true){
                 self.endSequence();
                 self.endTicker +=1;
@@ -37,34 +36,30 @@
             self.draw(screen);
             requestAnimationFrame(tick);
         };
-
         tick();
     };
 
     Game.prototype = {
         update: function(){
-            this.score = this.bodies.length - 1
+            this.score = this.bodies.length - 1;
             this.origin.update();
             for (var i = 0; i< this.bodies.length; i++){
                 this.bodies[i].update();
             }
             if (Math.random() > 0.99570 && this.fruit.length<10){
-                this.addFruit()
+                this.addFruit();
             }
         },
         draw: function(screen){
             screen.clearRect(3, 3, this.size.x-6, this.size.y-6);
-            screen.fillText(this.score, 30,40)
+            screen.fillText(this.score, 30,40);
             this.origin.draw(screen);
             for (var i = 0; i< this.bodies.length; i++){
-                this.bodies[i].draw(screen);
-                
+                this.bodies[i].draw(screen);        
             }
             for (var i = 0; i< this.fruit.length; i++){
-                this.fruit[i].draw(screen);
-                
+                this.fruit[i].draw(screen);                
             }
-            
         },
         addBody: function(body){
             this.bodies.unshift(body);
@@ -72,9 +67,8 @@
         addFruit: function(){
             var fruitCentre = {x: 10+ Math.random()*(this.size.x-20), y: 10+ Math.random()*(this.size.y-20)};
             var points = Math.floor( Math.random()*4 ) + 1 ;
-            var colour = randomColours[Math.floor(Math.random()*randomColours.length)]
+            var colour = randomColours[Math.floor(Math.random()*randomColours.length)];
             this.fruit.push( new Fruit(this, points, fruitCentre, colour, 0));
-
         },
         endSequence: function(){
             if (this.bodies.length>0) { 
@@ -83,9 +77,9 @@
             }
         },
         printGameOver: function(screen){
-                    screen.font = '40px Georgia';
-                    this.draw(screen)
-                    screen.fillText("GAME OVER", this.size.x/2, this.size.y/2);                    
+            screen.font = '40px Georgia';
+            this.draw(screen);
+            screen.fillText("GAME OVER", this.size.x/2, this.size.y/2);                    
         }
 
         
@@ -100,17 +94,17 @@
         this.game = game;
 
         this.size = { x: 10, y: 10};
-        this.polars = {r: 100, theta: 0}
-        this.centre = { x: this.game.size.x/2 +this.polars.r , y: this.game.size.y / 2}
-        this.colour = '#000000'
+        this.polars = {r: 100, theta: 0};
+        this.centre = { x: this.game.size.x/2 + this.polars.r , y: this.game.size.y/2};
+        this.colour = '#000000';
         
-        this.velocity = {r: 0, theta: 0.02}
+        this.velocity = {r: 0, theta: 0.02};
         this.keyboarder = new Keyboarder();
 
-        this.next = null
-        this.prev = null
+        this.next = null;
+        this.prev = null;
 
-        this.tracks = {x: [], y: []}
+        this.tracks = {x: [], y: []};
     };
 
     Player.prototype = {
@@ -149,34 +143,31 @@
             this.polars.r += this.velocity.r;
             this.polars.theta += this.velocity.theta;
         
-            this.toCartesian(this.polars.r, this.polars.theta) ;
+            this.toCartesian(this.polars.r, this.polars.theta);
 
             this.checkCollision();
             this.checkOutOfBounds();
-
 
         },
         draw: function(screen) {
             drawRect(screen, this, this.colour);
         },
-
         toCartesian: function(){
             this.centre.x = this.polars.r*Math.cos(this.polars.theta) + this.game.origin.centre.x;
             this.centre.y  = this.polars.r*Math.sin(this.polars.theta) + this.game.origin.centre.y;
         },
         storeInTracks: function(){
-            storeThisPosition(this)
+            storeThisPosition(this);
         },
-
         addTail: function(){
-            segment = this
-                while (true){
-                    if (segment.next === null){
-                        segment.next = new BodyPart(segment.game, segment);
-                        segment.game.addBody(segment.next);
-                        break
-                    }
-                    segment = segment.next
+            var segment = this;
+            while (true){
+                if (segment.next === null){ 
+                    segment.next = new BodyPart(segment.game, segment);
+                    segment.game.addBody(segment.next);
+                    break;
+                }
+                segment = segment.next;
                 }
         },
         checkCollision: function(){
@@ -187,16 +178,12 @@
                     }
                     this.game.fruit.splice(i,1);
                 }
-
             }
-            
-             
-
         },
         checkOutOfBounds: function(){
             if (this.centre.x < 0 || this.centre.x > this.game.size.x ||
                 this.centre.y < 0 || this.centre.y > this.game.size.y){
-                this.game.isOver = true
+                this.game.isOver = true;
             }
         }
     }
@@ -204,10 +191,8 @@
     
 
 
-    var BodyPart = function(game, previous){
-        
+    var BodyPart = function(game, previous){       
         this.game = game;
-
         this.size = {x:10, y:10};
         this.centre = {x:previous.tracks.x[0], y:previous.tracks.y[0]};
 
@@ -215,30 +200,26 @@
         this.next = null;
 
         this.tracks = {x:[], y:[]};
-        
     }
 
     BodyPart.prototype = {
-
         update: function(){
-        
-        this.storeInTracks()
-        this.centre.x = this.prev.tracks.x[0] ;
-        this.centre.y = this.prev.tracks.y[0] ;
-        
+        this.storeInTracks();
+        this.centre.x = this.prev.tracks.x[0];
+        this.centre.y = this.prev.tracks.y[0];
         },        
         draw: function(screen){
-            drawRect(screen, this)
+            drawRect(screen, this);
         },
         storeInTracks: function(){
-            storeThisPosition(this)
+            storeThisPosition(this);
         }
     }
 
     var Fruit = function (game, points, centre, colour, velocity){
         this.game = game; 
         this.points = points;
-        this.colour = colour
+        this.colour = colour;
         
         this.size = {x:9-this.points, y:9-this.points};
         this.centre = centre;
@@ -248,7 +229,6 @@
     Fruit.prototype = {
        draw: function(screen){
             drawRect(screen, this, this.colour)
-
        }
     }
 
@@ -259,7 +239,7 @@
        this.size = {x:1, y:1};
        this.centre = { x: this.game.size.x/2, y: this.game.size.y / 2}; 
 
-       this.colour = '#000000'  
+       this.colour = '#000000';  
     }
 
     Origin.prototype = {
@@ -285,7 +265,7 @@
         this.isDown = function(keyCode){
             return keyState[keyCode]===true;
         };
-        this.KEYS = {LEFT: 37, RIGHT:39, UP:38, DOWN:40, ENTER:13}
+        this.KEYS = {LEFT: 37, RIGHT:39, UP:38, DOWN:40, ENTER:13};
         }
 
     var storeThisPosition = function(segment){
@@ -300,12 +280,11 @@
 
 
     var drawRect = function(screen, body, colour){
-        var drawColour = colour
+        var drawColour = colour;
         if (colour === undefined){
-            drawColour = '#000000'
+            drawColour = '#000000';
         } else if (colour === 'random')
-            drawColour = randomColours[Math.floor(Math.random()*randomColours.length)]
-
+            drawColour = randomColours[Math.floor(Math.random()*randomColours.length)];
 
         screen.fillStyle= drawColour;
         screen.fillRect(body.centre.x - body.size.x/2,
@@ -321,17 +300,14 @@
             (bodyB.centre.x+bodyB.size.x/2 > bodyA.centre.x-bodyA.size.x/2) &&
             (bodyB.centre.y-bodyB.size.y/2 < bodyA.centre.y+bodyA.size.y/2) &&
             (bodyB.centre.y+bodyB.size.y/2 > bodyA.centre.y-bodyA.size.y/2) ){
-            console.log(bodyA.centre.x, bodyB.centre.x)
+            console.log(bodyA.centre.x, bodyB.centre.x);
             return true;
         }
         else
             return false;
     }
 
-
-
-    var randomColours = ['#5ABA47']//['#7bf6b6', '#00b8ff', '#fb9800', '#f28686', ] 
-
+    var randomColours = ['#5ABA47'];//['#7bf6b6', '#00b8ff', '#fb9800', '#f28686', ] 
 
     window.addEventListener('load', function(){
         new Game();
