60a3970ad159ae7194c857bf914b197be36cf5e8
Eric Hambro <eric.hambro@gmail.com>
2019-03-27 15:49:51 +0000

    clarify highscores

--- loadhighscores.js
+++ loadhighscores.js
@@ -14,15 +14,16 @@ var displayHighScores = function(){
     var name2 = document.createTextNode(sortedArray[1]+" : "+localStorage.getItem(sortedArray[1]));
     var name3 = document.createTextNode(sortedArray[2]+" : "+localStorage.getItem(sortedArray[2]));
  
-    if (sortedArray[0]!== undefined)
+    if (sortedArray[0]!== undefined && sortedArray[0]!== "-1" )
          high1.appendChild(name1);
-    if (sortedArray[1]!== undefined) 
+    if (sortedArray[1]!== undefined && sortedArray[1]!== "-1" ) 
         high2.appendChild(name2);
-    if (sortedArray[2]!== undefined)
+    if (sortedArray[2]!== undefined && sortedArray[2]!== "-1" )
         high3.appendChild(name3);
+    document.getElementById("playername").textContent = userName
 }
 
-var userName 
+var userName; 
 if (localStorage.getItem('-1') == null){
     var randomNameList = ['Leo Euler', 'Pedro Laplace', 'Joe Lagrange', 'Bill Hamilton','Mystery Person']
     userName = randomNameList[Math.floor(Math.random()*randomNameList.length)]
